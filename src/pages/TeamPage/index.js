65b48ce58d1d759f18119505c63880bc9fe413ae
GenryhK <kovalenko.genryh@gmail.com>
2019-02-01 19:36:24 +0200

    connect team page with store

--- src/pages/TeamPage/index.js
+++ src/pages/TeamPage/index.js
@@ -1,77 +1,86 @@
 import React, {Component} from 'react';
 import './index.css';
 import LeftBar from '../../components/LeftBar';
+import {connect} from 'react-redux'
 
 class TeamPage extends Component {
-  programersArr = [
-    {
-      id: 1,
-      name: 'Alexey',
-      email: 'alexeysergeev@gmail.com',
-      access: 'Admin'
-    },
-    {
-      id: 2,
-      name: 'Alexey',
-      email: 'alexeysergeev@gmail.com',
-      access: 'Admin'
-    },
-    {
-      id: 3,
-      name: 'Alexey',
-      email: 'alexeysergeev@gmail.com',
-      access: 'Admin'
-    }
-  ];
+    programersArr = [
+        {
+            id: 1,
+            name: 'Alexey',
+            email: 'alexeysergeev@gmail.com',
+            access: 'Admin'
+        },
+        {
+            id: 2,
+            name: 'Alexey',
+            email: 'alexeysergeev@gmail.com',
+            access: 'Admin'
+        },
+        {
+            id: 3,
+            name: 'Alexey',
+            email: 'alexeysergeev@gmail.com',
+            access: 'Admin'
+        }
+    ];
     headerItems = [
         'Name',
         'E-mail',
         'Access',
     ];
 
-  render() {
-    const headerItemsElements = this.headerItems.map( (element)=>
-        <th key={element}>{element}</th>
-    )
-    const items = this.programersArr.map((element) =>
-      <tr key={element.id}>
-        <td>{element.name}</td>
-        <td>{element.email}</td>
-        <td>
-          <div className="access_container">
-            {element.access}
-          </div>
-        </td>
-      </tr>
-    )
+    render() {
+        let programersArr = this.props.programersArr;
+        console.log(programersArr);
+        const headerItemsElements = this.headerItems.map((element) =>
+            <th key={element}>{element}</th>
+        )
+        const items = programersArr.map((element) =>
+            <tr key={element.id}>
+                <td>{element.name}</td>
+                <td>{element.email}</td>
+                <td>
+                    <div className="access_container">
+                        {element.access}
+                    </div>
+                </td>
+            </tr>
+        )
 
-    return (
-      <div className="wrapper_team_page">
-        <LeftBar/>
-        <div className="data_container_team_page">
-          <div className="team_page_header">
-            <div className="page_name">
-              Team
+        return (
+            <div className="wrapper_team_page">
+                <LeftBar/>
+                <div className="data_container_team_page">
+                    <div className="team_page_header">
+                        <div className="page_name">
+                            Team
+                        </div>
+                        <div className="invite_container">
+                            <input type="text"/>
+                            <button>Invite</button>
+                        </div>
+                    </div>
+                    <div className="team_page_data">
+                        <table>
+                            <thead>
+                            {headerItemsElements}
+                            </thead>
+                            <tbody>
+                            {items}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-            <div className="invite_container">
-              <input type="text"/>
-              <button>Invite</button>
-            </div>
-          </div>
-          <div className="team_page_data">
-            <table>
-              <thead>
-              {headerItemsElements}
-              </thead>
-              <tbody>
-              {items}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
-export default TeamPage;
+const mapStateToProps = store => {
+    return {
+        programersArr: store.teamPageReducer.programersArr
+    }
+};
+
+export default connect(mapStateToProps)(TeamPage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import {projectReducer} from './ProjectsReducer'
 import {mainPageReducer} from "./MainPageReducer";
+import {teamPageReducer} from "./TeamPageReducer";
 
 export const rootReducer = combineReducers({
     projectReducer: projectReducer,
     mainPageReducer: mainPageReducer,
+    teamPageReducer: teamPageReducer,
 });
