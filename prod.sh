e6f5e71b3054f77fe257a0b3d8e6cc2eeed47ea6
Alexander G <alexgarmatenko@gmail.com>
2019-04-19 16:31:05 +0300

    Update npm scripts

--- prod.sh
+++ prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker-compose up -d --build
 docker exec -ti lazy-time_nodejs npm install
+docker exec -ti lazy-time_nodejs bash -c 'npm run copy:libs'
 docker exec -ti lazy-time_nodejs bash -c 'npm run build'
