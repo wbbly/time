529337fd88dd002a723d8cb983de052b6c6e5554
Alexander G <alexgarmatenko@gmail.com>
2019-04-26 12:32:01 +0300

    Update prod.sh. Add production build

--- prod.sh
+++ prod.sh
@@ -2,4 +2,4 @@
 docker-compose up -d --build
 docker exec -ti lazy-time_nodejs npm install
 docker exec -ti lazy-time_nodejs bash -c 'npm run copy:libs'
-docker exec -ti lazy-time_nodejs bash -c 'npm run build'
+docker exec -ti lazy-time_nodejs bash -c 'npm run build -- --prod'
