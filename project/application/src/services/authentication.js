2ff8d1e5d79c35b86e5528bd11d8bc5de4f235d8
Alexander G <alexgarmatenko@gmail.com>
2019-05-06 18:24:33 +0300

    WOB-71 Update user roles uuid

--- project/application/src/services/authentication.js
+++ project/application/src/services/authentication.js
@@ -1,8 +1,8 @@
 import { getUserFromLocalStorage, getUserRoleTitleFromLocalStorage } from './userStorageService';
 
 export const ROLES = {
-    ROLE_USER: 'e1f1f00c-abee-448c-b65d-cdd51bb042f1',
-    ROLE_ADMIN: '449bca08-9f3d-4956-a38e-7b5de27bdc73',
+    ROLE_ADMIN: '00000000-0000-0000-0000-000000000000',
+    ROLE_USER: '00000000-0000-0000-0000-000000000001',
 };
 
 export function userLoggedIn() {
