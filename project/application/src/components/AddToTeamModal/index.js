b40b395ee76be2c89b1348785b87b4545ef2c02c
Alexander G <alexgarmatenko@gmail.com>
2019-05-23 21:16:08 +0300

    WOB-225 Modify the styles on invite user modal

--- project/application/src/components/AddToTeamModal/index.js
+++ project/application/src/components/AddToTeamModal/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
 
-import { getTimestamp } from '../../services/timeService';
 import { AppConfig } from '../../config';
 import { getUserIdFromLocalStorage } from '../../services/userStorageService';
 import { getCurrentTeamDataFromLocalStorage } from '../../services/teamStorageService';
@@ -67,30 +66,32 @@ class AddToTeamModal extends Component {
 
     render() {
         return (
-            <div className="add_to_team_modal_wrapper">
-                <div className="add_to_team_modal_data">
-                    <i onClick={e => this.closeModal()} />
-                    {/*<div className="add_to_team_modal_input_container">*/}
-                    {/*<div className="add_to_team_modal_input_title">Username</div>*/}
-                    {/*<input*/}
-                    {/*type="text"*/}
-                    {/*ref={input => {*/}
-                    {/*this.userName = input;*/}
-                    {/*}}*/}
-                    {/*className="add_to_team_modal_input"*/}
-                    {/*/>*/}
-                    {/*</div>*/}
-                    <div className="add_to_team_modal_input_container">
-                        <div className="add_to_team_modal_input_title">Email</div>
-                        <input
-                            type="text"
-                            ref={input => {
-                                this.email = input;
-                            }}
-                            className="add_to_team_modal_input"
-                        />
+            <div className="wrapper_add_user_modal">
+                <div className="add_user_modal_background" />
+                <div className="add_user_modal_container">
+                    <div className="add_user_modal_header">
+                        <div className="add_user_modal_header_title">Invite to team</div>
+                        <i className="add_user_modal_header_close" onClick={e => this.closeModal()} />
                     </div>
-                    <button onClick={e => this.addUser(this.email.value)}>Add user</button>
+                    <div className="add_user_modal_data">
+                        <div className="add_user_modal_data_input_container">
+                            <input
+                                type="text"
+                                ref={input => {
+                                    this.email = input;
+                                }}
+                                placeholder={'Email...'}
+                            />
+                        </div>
+                    </div>
+                    <div className="add_user_modal_button_container">
+                        <button
+                            className="add_user_modal_button_container_button"
+                            onClick={e => this.addUser(this.email.value)}
+                        >
+                            Add user
+                        </button>
+                    </div>
                 </div>
             </div>
         );
