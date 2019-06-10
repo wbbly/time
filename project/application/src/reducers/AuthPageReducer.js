39689b8d35031edd8aaa1cfbef133bceb5c26159
GenryhK <kovalenko.genryh@gmail.com>
2019-06-10 16:44:17 +0300

    add modal to reducer

--- project/application/src/reducers/AuthPageReducer.js
+++ project/application/src/reducers/AuthPageReducer.js
@@ -6,6 +6,8 @@ export function authPageReducer(state = initialState, action) {
     switch (action.type) {
         case 'TOGGLE_REGISTER_MODAL':
             return { ...state, registerModal: action.payload.registerModal };
+        case 'TOGGLE_FORGOT_PASSWORD_MODAL':
+            return { ...state, forgotPasswordModal: action.payload.forgotPasswordModal };
         default:
             return state;
     }
