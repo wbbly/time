4b8e4aa2b07ce289bbed413f32741bcfcdc0e838
GenryhK <kovalenko.genryh@gmail.com>
2019-06-11 14:59:46 +0300

    add user menu

--- project/application/src/components/UserMenu/index.js
+++ project/application/src/components/UserMenu/index.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 // Services
-import { getLoggedUserName } from '../../services/tokenStorageService';
+import {getLoggedUserName} from '../../services/tokenStorageService';
 
 // Styles
 import './index.css';
-import { logoutByUnauthorized } from '../../services/authentication';
+import {logoutByUnauthorized} from '../../services/authentication';
 
 class UserMenu extends Component {
     render() {
         const username = getLoggedUserName();
         return (
             <div className="wrapper_user_menu">
-                <div className="logout_container" onClick={e => this.logout()}>
-                    <div className="user_name">{username}</div>
-                    {/*<div>*/}
-                    {/*<i className="logout" />*/}
-                    {/*<span>Log out</span>*/}
-                    {/*</div>*/}
+                <div className="logout_container">
+                    <div className="user_name">
+                        {username}
+                        <i className="profile_user"></i>
+                    </div>
+                    <div className="user_setting_modal">
+                        <div onClick={e => this.logout()}>
+                            <i className="logout"/>
+                            <span>Log out</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
