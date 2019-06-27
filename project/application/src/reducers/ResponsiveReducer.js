7e9c92331ca53ebfec142a19c41308d9c8535c0e
Andrii Tykhyi <tykhyi.andrii@gmail.com>
2019-06-27 09:03:23 +0300

    added switch menu and set mobile reducers

--- project/application/src/reducers/ResponsiveReducer.js
+++ project/application/src/reducers/ResponsiveReducer.js
@@ -5,12 +5,22 @@ const initialState = {
         width: 0,
         height: 0,
     },
+    isShowMenu: false,
+    isMobile: false,
 };
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
         case types.SET_VIEWPORT_SIZE: {
             return { ...state, viewport: payload };
+        }
+
+        case types.SWITCH_MENU: {
+            return { ...state, isShowMenu: !state.isShowMenu };
+        }
+
+        case types.SET_IS_MOBILE: {
+            return { ...state, isMobile: payload };
         }
 
         default:
