8a2b832083a1b2ed90b3c429da0006e9467bae5a
a-silent <tykhyi.andrii@gmail.com>
2019-07-12 13:21:57 +0300

    deleted switch language action

--- project/application/src/actions/LanguageActions.js
+++ project/application/src/actions/LanguageActions.js
@@ -1,11 +1,6 @@
 export const SET_LANGUAGE = 'SET_LANGUAGE';
-export const SWITCH_LANGUAGE = 'SWITCH_LANGUAGE';
 
 export const setLanguage = payload => ({
     type: SET_LANGUAGE,
     payload,
-});
-
-export const switchLanguage = () => ({
-    type: SWITCH_LANGUAGE,
 });
