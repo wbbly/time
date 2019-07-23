de5d0ab094316266001e2b6ce48a5584ecb6f1b9
Alexander Pokryshka <alexandr.pokrishka@lazy-ants.de>
2019-07-23 15:06:32 +0300

    Modal removed to PageTemplate, style fixed

--- project/application/src/components/ModalInfo/index.js
+++ project/application/src/components/ModalInfo/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import classNames from 'classnames';
+import noConnection from '../../images/icons/no-wifi.svg';
+import connectionRestored from '../../images/icons/wifi.svg';
 
 import './style.scss';
 
@@ -11,7 +13,13 @@ class ModalInfo extends Component {
                 className={classNames('modal-info', { 'modal-info--hidden': !this.props.modalInfoVisible })}
                 onClick={this.props.onClick}
             >
-                <div className={classNames('modal-info-icon', `modal-info-icon--${this.props.modalInfoType}`)} />
+                <div className={classNames('modal-info-icon', `modal-info-icon--${this.props.modalInfoType}`)}>
+                    {this.props.modalInfoType === 'lost-connection' ? (
+                        <img src={noConnection} />
+                    ) : this.props.modalInfoType === 'connection-restored' ? (
+                        <img src={connectionRestored} />
+                    ) : null}
+                </div>
                 <div className="modal-info-text">
                     <p>{this.props.modalInfoText}</p>
                 </div>
