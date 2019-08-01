1edf168f5e73289e10c56a6873f1162cc3e7e939
a-silent <tykhyi.andrii@gmail.com>
2019-08-01 11:12:39 +0300

    deleted SET_TABLE_DATA from team page reducer

--- project/application/src/actions/TeamPageAction.js
+++ project/application/src/actions/TeamPageAction.js
@@ -1,10 +1,5 @@
 export default function teamPageAction(actionType, action) {
-    if (actionType === 'SET_TABLE_DATA') {
-        return {
-            type: 'SET_TABLE_DATA',
-            payload: action,
-        };
-    } else if (actionType === 'TOGGLE_ADD_USER_MODAL') {
+    if (actionType === 'TOGGLE_ADD_USER_MODAL') {
         return {
             type: 'TOGGLE_ADD_USER_MODAL',
             payload: action,
