0361b97f36baf2bf83fc1ed02d88d021a02dd5f0
a-silent <tykhyi.andrii@gmail.com>
2019-08-01 10:34:47 +0300

    deleted unused team action

--- project/application/src/actions/TeamAction.js
+++
@@ -1,8 +0,0 @@
-export default function teamAction(actionType, action) {
-    if (actionType === 'SET_CURRENT_TEAM') {
-        return {
-            type: 'SET_CURRENT_TEAM',
-            payload: action,
-        };
-    }
-}
