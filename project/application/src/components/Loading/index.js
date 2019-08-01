a10d91c1383f1f554fd0c2accf48e3f9fed00e98
a-silent <tykhyi.andrii@gmail.com>
2019-08-01 11:00:52 +0300

    expanded loading component

--- project/application/src/components/Loading/index.js
+++ project/application/src/components/Loading/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 
 import Spinner from '../Spinner';
 
-export const Loading = ({ flag, children }) => (flag ? <Spinner /> : children());
+export const Loading = ({ flag, children, mode, withLogo }) =>
+    flag ? <Spinner mode={mode} withLogo={withLogo} /> : children;
