3357e7e9515dab9adef5f8fdaeca0bb3be209222
a-silent <tykhyi.andrii@gmail.com>
2019-08-02 16:24:06 +0300

    fixed bug with timer in sidebar

--- project/application/src/components/LeftBar/index.js
+++ project/application/src/components/LeftBar/index.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 
 import classNames from 'classnames';
+import openSocket from 'socket.io-client';
+import * as moment from 'moment';
 
 // Services
 import { getTimeDiff } from '../../services/timeService';
-import { getCurrentTimerFromLocalStorage } from '../../services/currentTimerStorageService';
+import { getTokenFromLocalStorage } from '../../services/tokenStorageService';
+import { apiCall } from '../../services/apiService';
+import { decodeTimeEntryIssue } from '../../services/timeEntryService';
+import { logoutByUnauthorized } from '../../services/authentication';
 
 // Components
 import TeamSwitcher from '../TeamSwitcher';
 import UserMenu from '../UserMenu';
 
 // Actions
+import {
+    setServerClientTimediffAction,
+    setCurrentTimerAction,
+    resetCurrentTimerAction,
+} from '../../actions/MainPageAction';
 
 // Queries
 
 // Config
+import { AppConfig } from '../../config';
 
 // Styles
 import './style.scss';
@@ -23,28 +35,98 @@ import './style.scss';
 class LeftBar extends Component {
     ONE_SECOND_PERIOD = 1000; // in ms
 
-    componentDidMount() {
-        this.setState({ timeEntriesList: this.props.timeEntriesList });
-        this.activeSmalltimer();
-    }
-
     state = {
         timer: '',
     };
+
+    initSocketConnection = () => {
+        const { setServerClientTimediffAction, setCurrentTimerAction, resetCurrentTimerAction } = this.props;
+
+        this.socketConnection = openSocket(AppConfig.apiURL);
+        this.socketConnection.on('connect', () => {
+            this.socketConnection.emit(
+                'join-v2',
+                {
+                    token: `Bearer ${getTokenFromLocalStorage()}`,
+                },
+                _ => {
+                    this.socketConnection.emit('check-timer-v2', {
+                        token: `Bearer ${getTokenFromLocalStorage()}`,
+                    });
+                }
+            );
+        });
+
+        this.socketConnection.on('check-timer-v2', data => {
+            if (data) {
+                apiCall(AppConfig.apiURL + 'time/current', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }).then(
+                    result => {
+                        setServerClientTimediffAction(+moment(result.timeISO) - +moment());
+                        const currentTimer = {
+                            timeStart: +moment(data.startDatetime),
+                            issue: decodeTimeEntryIssue(data.issue),
+                            project: data.project,
+                        };
+                        setCurrentTimerAction(currentTimer);
+                    },
+                    err => {
+                        if (err instanceof Response) {
+                            err.text().then(errorMessage => console.log(errorMessage));
+                        } else {
+                            console.log(err);
+                        }
+                    }
+                );
+            } else {
+                resetCurrentTimerAction();
+            }
+        });
+        this.socketConnection.on('stop-timer-v2', data => {
+            resetCurrentTimerAction();
+        });
+        this.socketConnection.on('user-unauthorized', data => logoutByUnauthorized());
+    };
+
+    componentDidMount() {
+        const { location } = this.props;
+        if (location.pathname !== '/timer') {
+            this.initSocketConnection();
+        }
+        this.activeSmalltimer();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.location.pathname !== this.props.location.pathname) {
+            this.props.location.pathname === '/timer'
+                ? this.socketConnection && this.socketConnection.emit('leave') && this.socketConnection.close()
+                : this.socketConnection
+                    ? !this.socketConnection.connected && this.initSocketConnection()
+                    : this.initSocketConnection();
+        }
+    }
+
+    componentWillUnmount() {
+        this.socketConnection && this.socketConnection.emit('leave') && this.socketConnection.close();
+    }
 
     activeSmalltimer() {
         setInterval(() => this.getTimeNow(), this.ONE_SECOND_PERIOD);
     }
 
     getTimeNow() {
-        let timer = getCurrentTimerFromLocalStorage();
-        if (!timer.timeStart) {
+        const { currentTimer } = this.props;
+        if (!currentTimer.timeStart) {
             this.setState({ timer: '' });
 
             return false;
         }
 
-        this.setState({ timer: getTimeDiff(timer.timeStart, true) });
+        this.setState({ timer: getTimeDiff(currentTimer.timeStart, true) });
     }
 
     visualTimer() {
@@ -100,4 +182,19 @@ class LeftBar extends Component {
     }
 }
 
-export default LeftBar;
+const mapStateToProps = state => ({
+    currentTimer: state.mainPageReducer.currentTimer,
+});
+
+const mapDispatchToProps = {
+    setServerClientTimediffAction,
+    setCurrentTimerAction,
+    resetCurrentTimerAction,
+};
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(LeftBar)
+);
