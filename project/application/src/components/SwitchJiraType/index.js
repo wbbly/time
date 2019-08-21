9d703f17be779642bca745a3143bcce9b575a159
Alexander G <alexgarmatenko@gmail.com>
2019-08-21 18:28:50 +0300

    Added jira-sync with type and url

---
+++ project/application/src/components/SwitchJiraType/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+import classNames from 'classnames';
+
+// styles
+import './style.scss';
+
+const jiraType = [
+    {
+        type: 'cloud',
+        name: 'Jira Cloud',
+    },
+    {
+        type: 'self',
+        name: 'Jira self-hosted',
+    },
+];
+
+class SwitchJiraType extends Component {
+    state = {
+        isOpenDropdown: false,
+        selectedItem: null,
+    };
+
+    closeDropdown = event => {
+        this.setState({ isOpenDropdown: false });
+        document.removeEventListener('click', this.closeDropdown);
+    };
+
+    openDropdown = event => {
+        document.addEventListener('click', this.closeDropdown);
+        this.setState({ isOpenDropdown: true });
+    };
+    componentWillMount() {
+        if (this.props.selectedType) {
+            jiraType.forEach((item, index) => {
+                if (item.type === this.props.selectedType) {
+                    this.setState({ selectedItem: jiraType[index] });
+                }
+            });
+            return;
+        }
+        this.setState({ selectedItem: jiraType[0] });
+        this.props.onSelect(jiraType[0].type);
+    }
+    render() {
+        const { dropdown, isMobile } = this.props;
+        const { isOpenDropdown, selectedItem } = this.state;
+
+        return (
+            <div
+                className={classNames('wrapper-switch-type', {
+                    'wrapper-switch-type--dropdown': dropdown,
+                    'wrapper-switch-type--block': !dropdown,
+                    'wrapper-switch-type--mobile': isMobile,
+                })}
+            >
+                <div className="wrapper-switch-type__title">Jira type</div>
+                <div className="wrapper-switch-type__select" onClick={this.openDropdown}>
+                    {selectedItem.name}
+
+                    {isOpenDropdown && (
+                        <div className="wrapper-switch-type__list">
+                            {jiraType.map(item => (
+                                <div
+                                    key={item.name}
+                                    className="wrapper-switch-type__list-item"
+                                    onClick={() => {
+                                        this.setState({ selectedItem: item });
+                                        this.props.onSelect(item.type);
+                                    }}
+                                >
+                                    {item.name}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    <i className="wrapper-switch-type__icon-arrow" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SwitchJiraType;
