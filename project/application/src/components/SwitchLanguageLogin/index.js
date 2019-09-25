9bcd02911613ffbcbcb631dcc6ac83cdc25a70c1
GenryhK <kovalenko.genryh@gmail.com>
2019-09-25 19:46:16 +0300

    fix component

---
+++ project/application/src/components/SwitchLanguageLogin/index.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import classNames from 'classnames';
+
+// actions
+import { setLanguage } from '../../actions/LanguageActions';
+
+// styles
+import './style.scss';
+
+class SwitchLanguageLogin extends Component {
+    state = {
+        isOpenDropdown: false,
+    };
+
+    isActive = language => {
+        const { vocabulary } = this.props;
+        const { lang } = vocabulary;
+
+        return lang.short === language;
+    };
+
+    closeDropdown = event => {
+        this.setState({ isOpenDropdown: false });
+        document.removeEventListener('click', this.closeDropdown);
+    };
+
+    openDropdown = event => {
+        document.addEventListener('click', this.closeDropdown);
+        this.setState({ isOpenDropdown: true });
+    };
+    render() {
+        let { dropdown, setLanguage, isMobile, vocabulary, languages } = this.props;
+        const { isOpenDropdown } = this.state;
+        const { lang } = vocabulary;
+
+        return (
+            <div
+                className={classNames('wrapper-switch-language-login', {
+                    'wrapper-switch-language--dropdown': dropdown,
+                    'wrapper-switch-language--block': !dropdown,
+                    'wrapper-switch-language--mobile': isMobile,
+                })}
+            >
+                {dropdown ? (
+                    <>
+                        <div className="wrapper-switch-language__select" onClick={this.openDropdown}>
+                            <span>{lang.long}</span>
+
+                            {isOpenDropdown && (
+                                <div className="wrapper-switch-language__list">
+                                    {languages.map(item => (
+                                        <div
+                                            key={item.long}
+                                            className="wrapper-switch-language__list-item"
+                                            onClick={event => {
+                                                setLanguage(item.short);
+                                            }}
+                                        >
+                                            {item.long}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            <i className="wrapper-switch-language__icon-arrow" />
+                        </div>
+                    </>
+                ) : (
+                    <ul className="wrapper-switch-language__list">
+                        {languages.map(item => (
+                            <li
+                                key={item.short}
+                                className={classNames('wrapper-switch-language__list-item', {
+                                    'wrapper-switch-language__list-item--active': this.isActive(item.short),
+                                })}
+                                onClick={event => setLanguage(item.short)}
+                            >
+                                {item.short}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    vocabulary: state.languageReducer.vocabulary,
+    languages: state.languageReducer.languages,
+    isMobile: state.responsiveReducer.isMobile,
+});
+
+const mapDispatchToProps = {
+    setLanguage,
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SwitchLanguageLogin);
