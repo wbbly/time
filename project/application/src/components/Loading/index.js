5d061670141cdfbc06373e91ad496d3565b73dbf
a-silent <tykhyi.andrii@gmail.com>
2019-11-24 19:11:56 +0200

    update Loading component
    - added mode overlay
    - added circle key

--- project/application/src/components/Loading/index.js
+++ project/application/src/components/Loading/index.js
@@ -2,5 +2,28 @@ import React from 'react';
 
 import Spinner from '../Spinner';
 
-export const Loading = ({ flag, children, mode, withLogo }) =>
-    flag ? <Spinner mode={mode} withLogo={withLogo} /> : children;
+export const Loading = ({ flag, children, mode, withLogo, width, height, circle }) => {
+    if (mode === 'overlay' && width && height) {
+        const style = width && height ? { width, height } : {};
+
+        return flag ? (
+            <div style={{ display: 'inline-block', position: 'relative', ...style }}>
+                <Spinner mode={mode} circle={circle} withLogo={withLogo} />
+                {children}
+            </div>
+        ) : (
+            children
+        );
+    } else if (mode === 'overlay') {
+        return flag ? (
+            <>
+                <Spinner mode={mode} withLogo={withLogo} />
+                {children}
+            </>
+        ) : (
+            children
+        );
+    } else {
+        return flag ? <Spinner mode={mode} withLogo={withLogo} /> : children;
+    }
+};
