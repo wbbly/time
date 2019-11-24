868b48ea3e0aea35a888a78ebf443e42922fad76
a-silent <tykhyi.andrii@gmail.com>
2019-11-24 18:11:51 +0200

    update JiraIcon component
    - replaced apiCall request on axios request
    - added getting new tasks list after succesfull sync task with jira

--- project/application/src/components/JiraIcon/index.js
+++ project/application/src/components/JiraIcon/index.js
@@ -3,48 +3,37 @@ import { connect } from 'react-redux';
 
 import classNames from 'classnames';
 
-// Services
-import { apiCall } from '../../services/apiService';
-import { getTokenFromLocalStorage } from '../../services/tokenStorageService';
-
 // Config
-import { AppConfig } from '../../config';
+import { syncTaskWithJira } from '../../configAPI';
 
 // Actions
 import { showNotificationAction } from '../../actions/NotificationActions';
+import { getTimeEntriesListAction } from '../../actions/MainPageAction';
 
 // Styles
 import './style.scss';
-import { type } from 'os';
 
 class JiraIcon extends Component {
     state = {
         rotateJiraIcon: false,
-        isSyncOk: false,
     };
 
-    syncTaskWithJira = event => {
-        const { vocabulary, taskData, isUpdatingTask, showNotificationAction } = this.props;
-        const { id } = taskData;
+    syncTaskWithJira = async event => {
+        const { vocabulary, task, disableClick, showNotificationAction, getTimeEntriesListAction } = this.props;
+        const { id } = task;
 
-        if (!isUpdatingTask) {
+        if (!disableClick) {
             this.setState({ rotateJiraIcon: true });
-            apiCall(AppConfig.apiURL + `sync/jira/issue/${id}/worklog`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `'Bearer ${getTokenFromLocalStorage()}'`,
-                },
-            }).then(
-                result => {
-                    this.setState({ rotateJiraIcon: false, isSyncOk: true });
-                },
-                err =>
-                    err.text().then(text => {
-                        this.setState({ rotateJiraIcon: false });
-                        showNotificationAction({ text: vocabulary[JSON.parse(text).message], type: 'error' });
-                    })
-            );
+            try {
+                await syncTaskWithJira(id);
+                await getTimeEntriesListAction();
+            } catch (error) {
+                if (error.response.data.message) {
+                    showNotificationAction({ text: vocabulary[error.response.data.message], type: 'error' });
+                }
+            } finally {
+                this.setState({ rotateJiraIcon: false });
+            }
         }
     };
 
@@ -52,14 +41,14 @@ class JiraIcon extends Component {
         const { isSync, user } = this.props;
         const { tokenJira } = user;
 
-        const { rotateJiraIcon, isSyncOk } = this.state;
+        const { rotateJiraIcon } = this.state;
 
         return tokenJira ? (
             <div
                 className={classNames('wrapper-jira-icon', { 'wrapper-jira-icon--rotate-jira': rotateJiraIcon })}
                 onClick={this.syncTaskWithJira}
             >
-                <i className={classNames('jira-icon', { 'jira-icon-is-sync': isSync || isSyncOk })} />
+                <i className={classNames('jira-icon', { 'jira-icon-is-sync': isSync })} />
             </div>
         ) : null;
     }
@@ -72,6 +61,7 @@ const mapStatetToProps = state => ({
 
 const mapDispatchToProps = {
     showNotificationAction,
+    getTimeEntriesListAction,
 };
 
 export default connect(
