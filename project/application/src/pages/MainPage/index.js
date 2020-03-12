4cac02488d2fc8a49a66c752538c163010bdc37f
UlkoSergii <ulkosergii@gmail.com>
2020-03-12 16:00:48 +0200

    implemented virtualized scroll

--- project/application/src/pages/MainPage/index.js
+++ project/application/src/pages/MainPage/index.js
@@ -5,6 +5,8 @@ import moment from 'moment';
 import classNames from 'classnames';
 
 import { getDateInString } from '../../services/timeService';
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+import List from 'react-virtualized/dist/commonjs/List';
 
 // Components
 import { Loading } from '../../components/Loading';
@@ -21,11 +23,14 @@ import { getProjectsListActions } from '../../actions/ProjectsActions';
 
 // Styles
 import './style.scss';
+import { WindowScroller } from 'react-virtualized';
 
 class MainPage extends Component {
     state = {
         isInitialFetching: true,
     };
+
+    listRef = React.createRef();
 
     splitTimersByDay = (timers = []) => {
         const formattedLogsDates = [];
@@ -70,6 +75,50 @@ class MainPage extends Component {
         return getDateInString(totalTime, durationTimeFormat);
     };
 
+    _getDatum = index => {
+        const { timeEntriesList } = this.props;
+        const list = this.splitTimersByDay(timeEntriesList);
+        return list[index];
+    };
+
+    _getRowHeight = ({ index }) => {
+        //todo new algorithm needed
+        const datumLength = this._getDatum(index).length;
+        const containerSize = 36;
+        const marginBottomSize = 26;
+        const listItemSize = 58;
+        return containerSize + marginBottomSize + listItemSize * datumLength;
+    };
+
+    _rowRenderer = ({ index, key, style, length }) => {
+        const { vocabulary } = this.props;
+        const { v_total_time } = vocabulary;
+
+        const datum = this._getDatum(index);
+
+        let additionalContent;
+
+        return (
+            <div key={key} style={style}>
+                <div
+                    className={classNames('main-page__day', {
+                        'main-page__day--last-child': index === length - 1,
+                    })}
+                >
+                    <div className="main-page__day-header">
+                        <div className="main-page__day-date">{this.renderDayDateString(datum[0].startDatetime)}</div>
+                        <div className="main-page__day-date-all-time">
+                            {v_total_time}: {this.renderTotalTimeByDay(datum)}
+                        </div>
+                    </div>
+                    {datum.map(task => (
+                        <TaskListItem key={task.id} task={task} />
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
     async componentDidMount() {
         const { getTimeEntriesListAction, getProjectsListActions } = this.props;
         await getTimeEntriesListAction();
@@ -80,14 +129,19 @@ class MainPage extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        const { currentTimer, getTimeEntriesListAction } = this.props;
+        const { currentTimer, getTimeEntriesListAction, timeEntriesList } = this.props;
         // getting a new task list when stopping or starting
         // a new task without stopping the last task
+
         if (
             (prevProps.currentTimer && !currentTimer) ||
             (prevProps.currentTimer && currentTimer && prevProps.currentTimer.id !== currentTimer.id)
         ) {
             getTimeEntriesListAction();
+        }
+
+        if (prevProps.timeEntriesList.length !== timeEntriesList.length) {
+            if (this.listRef && this.listRef.current) this.listRef.current.recomputeRowHeights(0);
         }
     }
 
@@ -108,6 +162,7 @@ class MainPage extends Component {
         } = this.props;
         const { v_total_time } = vocabulary;
         const { isInitialFetching } = this.state;
+        const dividedTimersByDay = this.splitTimersByDay(timeEntriesList);
         return (
             <Loading flag={isInitialFetching} mode="parentSize" withLogo={false}>
                 {user.onboardingMobile ? (
@@ -122,42 +177,79 @@ class MainPage extends Component {
                     >
                         <AddTask />
                         <CustomScrollbar>
-                            <div className="main-page__list">
-                                {this.splitTimersByDay(timeEntriesList).map((day, index, arr) => (
-                                    <div
-                                        className={classNames('main-page__day', {
-                                            'main-page__day--last-child': index === arr.length - 1,
-                                        })}
-                                        key={index}
-                                    >
-                                        <div className="main-page__day-header">
-                                            <div className="main-page__day-date">
-                                                {this.renderDayDateString(day[0].startDatetime)}
-                                            </div>
-                                            <div className="main-page__day-date-all-time">
-                                                {v_total_time}: {this.renderTotalTimeByDay(day)}
-                                            </div>
-                                        </div>
-                                        {day.map(task => (
-                                            <TaskListItem key={task.id} task={task} />
-                                        ))}
-                                    </div>
-                                ))}
-                                {isFetchingTimeEntriesList && (
-                                    <Loading
-                                        mode="overlay"
-                                        withLogo={false}
-                                        flag={isFetchingTimeEntriesList}
-                                        width="100%"
-                                        height="100%"
-                                    >
-                                        <div className="main-page__lazy-load-spinner" />
-                                    </Loading>
+                            <WindowScroller>
+                                {({ height }) => (
+                                    <AutoSizer>
+                                        {({ width }) => [
+                                            <List
+                                                ref={this.listRef}
+                                                className="main-page__list"
+                                                height={height}
+                                                autoHeight
+                                                overscanRowCount={10}
+                                                noRowsRenderer={<div />}
+                                                rowCount={dividedTimersByDay.length}
+                                                rowHeight={this._getRowHeight}
+                                                rowRenderer={props =>
+                                                    this._rowRenderer({
+                                                        ...props,
+                                                        length: dividedTimersByDay.length,
+                                                    })
+                                                }
+                                                width={width - 17} //todo calculate width by another way
+                                            />,
+                                            isFetchingTimeEntriesList && (
+                                                <Loading
+                                                    mode="overlay"
+                                                    withLogo={false}
+                                                    flag={isFetchingTimeEntriesList}
+                                                    width={width - 17} //todo calculate width by another way
+                                                    height={100}
+                                                >
+                                                    <div className="main-page__lazy-load-spinner" />
+                                                </Loading>
+                                            ),
+                                        ]}
+                                    </AutoSizer>
                                 )}
-                                {isMobile &&
-                                    !currentTimer &&
-                                    pagination.disabled && <div className="main-page__empty-block" />}
-                            </div>
+                            </WindowScroller>
+
+                            {/*<div className="main-page__list">*/}
+                            {/*    {this.splitTimersByDay(timeEntriesList).map((day, index, arr) => (*/}
+                            {/*        <div*/}
+                            {/*            className={classNames('main-page__day', {*/}
+                            {/*                'main-page__day--last-child': index === arr.length - 1,*/}
+                            {/*            })}*/}
+                            {/*            key={index}*/}
+                            {/*        >*/}
+                            {/*            <div className="main-page__day-header">*/}
+                            {/*                <div className="main-page__day-date">*/}
+                            {/*                    {this.renderDayDateString(day[0].startDatetime)}*/}
+                            {/*                </div>*/}
+                            {/*                <div className="main-page__day-date-all-time">*/}
+                            {/*                    {v_total_time}: {this.renderTotalTimeByDay(day)}*/}
+                            {/*                </div>*/}
+                            {/*            </div>*/}
+                            {/*            {day.map(task => (*/}
+                            {/*                <TaskListItem key={task.id} task={task} />*/}
+                            {/*            ))}*/}
+                            {/*        </div>*/}
+                            {/*    ))}*/}
+                            {/*    {isFetchingTimeEntriesList && (*/}
+                            {/*        <Loading*/}
+                            {/*            mode="overlay"*/}
+                            {/*            withLogo={false}*/}
+                            {/*            flag={isFetchingTimeEntriesList}*/}
+                            {/*            width="100%"*/}
+                            {/*            height="100%"*/}
+                            {/*        >*/}
+                            {/*            <div className="main-page__lazy-load-spinner" />*/}
+                            {/*        </Loading>*/}
+                            {/*    )}*/}
+                            {/*    {isMobile &&*/}
+                            {/*        !currentTimer &&*/}
+                            {/*        pagination.disabled && <div className="main-page__empty-block" />}*/}
+                            {/*</div>*/}
                         </CustomScrollbar>
                         <StartTaskMobile />
                     </div>
